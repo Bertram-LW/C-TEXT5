a00fb6186ac0a8ba85b0265425331187b22b9f56
Bertram-LW <58548593+Bertram-LW@users.noreply.github.com>
2020-05-22 11:34:13 +0800

    text5
    记事本打开

---
+++ NUM2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace NUM2
+{
+
+     sealed class information
+    {
+        private string ID="";
+        private string cipher="";
+
+        public string UserId
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+            }
+        }
+        public string UserCipher
+        {
+            get
+            {
+                return cipher;
+            }
+            set
+            {
+                cipher = value;
+            }
+        }
+        public void showinformation()
+        {
+            Console.WriteLine("用户名：" + ID);
+            Console.WriteLine("密  码：" + cipher);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            information i = new information();
+            Console.WriteLine("请输入用户名：");
+            i.UserId = Console.ReadLine();
+            Console.WriteLine("请输入密码：");
+            i.UserCipher= Console.ReadLine();
+            i.showinformation();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Intrinsics.X86;
+
+namespace TEXT5one
+{
+    public  abstract class Area
+    {
+        public abstract void GetArea();
+        
+    }
+    public  sealed class Circle : Area
+    {    double R;
+        
+        public Double GetR()
+        {
+            Console.WriteLine("请输入圆的半径：");
+            return Double.Parse(Console.ReadLine());
+        }
+        public override void GetArea()
+        {
+            R = GetR();
+            Console.WriteLine("圆的面积：" + Math.PI * R * R);          
+        }
+    }
+    public class Rectangle : Area
+    {
+        double length;
+        double width;
+        public Double GetLength()
+        {
+            Console.WriteLine("请输入矩形的长：");
+            return Double.Parse(Console.ReadLine());
+        }
+        public Double GetWidth()
+        {
+            Console.WriteLine("请输入矩形的宽：");
+            return Double.Parse(Console.ReadLine());
+
+        }
+        public sealed override void GetArea()
+
+        {
+            length = GetLength();
+            width = GetWidth();
+            Console.WriteLine("矩形的面积：" + length * width);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            c.GetArea();
+            Rectangle r = new Rectangle();
+            r.GetArea();
+
+
+        }
+    }
+}
